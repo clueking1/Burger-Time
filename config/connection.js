9cd0a95f2f77638225bd6e2808b36cedc43ef3eb
Will Coan <wcoan300@gmail.com>
2020-02-22 18:20:05 -0500

    added JAWS DB port

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,15 @@
 const mysql = require('mysql')
-
+if(process.env.JAWSDB_URL) {
+    const con = mysql.createConnection(process.env.JAWSDB_URL)
+} else {
 const con = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: 'password',
+    password: '',
     database: 'burgers_db'
 })
+}
 
 con.connect(err => {
     if (err) {
